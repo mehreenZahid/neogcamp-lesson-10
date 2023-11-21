2812475f7fb94a589e507443ee96d1d3cbb6a237
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-11-21 19:20:34 +0530

    js part done

--- script.js
+++ script.js
@@ -4,22 +4,34 @@ var currentPrice = document.querySelector("#current-price")
 var submitBtn = document.querySelector("#submit-btn")
 var outputBox = document.querySelector("#output-box")
 
+submitBtn.addEventListener('click', submitHandler)
+
+function submitHandler(){
+    var ip = Number(initialPrice.value)
+    var qty = Number(stockQuantity.value)
+    var curr = Number(currentPrice.value)
+
+    calculateProfitAndLoss(ip, qty, curr)
+}
+
 function calculateProfitAndLoss(initial, quantity, current){
     if(initial > current){
         var loss = (initial - current)*quantity
         var lossPercentage = (loss/initial)*100
 
-        console.log(`Your loss is ${loss} and the loss percentage is ${lossPercentage}%`)
+        showOutput(`Your loss is ${loss} and the loss percentage is ${lossPercentage}%`)
     }
     else if(current > initial){
         var profit = (current - initial)*quantity
         var profitPercentage = (profit/initial)*100
 
-        console.log(`Your profit is ${profit} and the profit percentage is ${profitPercentage}%`)
+        showOutput(`Your profit is ${profit} and the profit percentage is ${profitPercentage}%`)
     }
     else{
-        console.log("No profit or loss")
+        showOutput("No profit or loss")
     }
 }
 
-calculateProfitAndLoss(100, 10, 400)
+function showOutput(message){
+    outputBox.innerText = message
+}
